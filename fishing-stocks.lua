c24966a2f15a40b5c63456c7ad94eaa70df8ba8b
jgfhkjtdyj <mxstr73@gmail.com>
2025-02-28 20:26:57 +0300

    Update fishing-stocks.lua

--- fishing-stocks.lua
+++ fishing-stocks.lua
@@ -4,6 +4,8 @@ local u8 = encoding.UTF8;
 
 encoding.default = 'cp1251';
 
+local currentSkill = -1;
+
 local PRICE_MAP = {
   ['Малоротый окунь'] = {100, 300},
   ['Радужная форель'] = {200, 400},
@@ -29,12 +31,20 @@ local PRICE_MAP = {
 };
 
 function main()
+  while sampIsLocalPlayerSpawned() and not isInitialized() do
+    wait(5000);
+    sampSendChat('/fish skill');
+    wait(5000);
+  end
+
   wait(-1);
 end
 
 function hook.onShowDialog(id, style, title, button, button2, text)
-  if isSRP() and title:find('Рыболовный магазин') then
-    if title:find('Рыболовные товары') then
+  if isSRP() then
+    local status = isInitialized();
+
+    if status and title:find('Рыболовный магазин') and title:find('Рыболовные товары') then
       local resultText = {};
 
       for i, line in ipairs(splitDialogBody(text)) do
@@ -49,8 +59,9 @@ function hook.onShowDialog(id, style, title, button, button2, text)
           local u8name = u8:encode(name);
 
           if u8name and PRICE_MAP[u8name] then
-            local minPrice = PRICE_MAP[u8name][1] * 1.3;
-            local maxPrice = PRICE_MAP[u8name][2] * 1.3;
+            local shopBuff = getSkillShopBuff();
+            local minPrice = PRICE_MAP[u8name][1] * shopBuff;
+            local maxPrice = PRICE_MAP[u8name][2] * shopBuff;
             local percent = math.floor(((currentPrice - minPrice) / (maxPrice - minPrice)) * 100);
 
             line = line:gsub(
@@ -65,7 +76,53 @@ function hook.onShowDialog(id, style, title, button, button2, text)
 
       return {id, style, title, button, button2, table.concat(resultText, '\n')};
     end
+
+    if not status and title:find('Навык') and title:find('Рыбалка') and button:equals('Бонусы') then
+      currentSkill = tonumber(text:match('Текущий уровень: {FFFFFF}(%d+)'));
+
+      return false;
+    end
   end
+end
+
+function hook.onServerMessage(color, text)
+  if isSRP() then
+    if color == 1790050303 and text:find('Навык рыбной ловли повышен до') then
+      currentSkill = tonumber(msg:match('.*Навык рыбной ловли повышен до {.*}(%d+)'));
+    end
+  end
+end
+
+function getSkillShopBuff()
+  if currentSkill >= 10 then
+    return 1.30;
+  end
+
+  if currentSkill >= 9 then
+    return 1.26;
+  end
+
+  if currentSkill >= 8 then
+    return 1.22;
+  end
+
+  if currentSkill >= 7 then
+    return 1.18;
+  end
+
+  if currentSkill >= 5 then
+    return 1.15;
+  end
+
+  if currentSkill >= 3 then
+    return 1.07;
+  end
+
+  if currentSkill >= 1 then
+    return 1.03;
+  end
+
+  return 1;
 end
 
 function formatPercent(value)
@@ -100,6 +157,10 @@ function splitDialogBody(body)
   return result;
 end
 
+function isInitialized()
+  return currentSkill >= 0;
+end
+
 
 local find = string.find;
 local match = string.match;
@@ -115,4 +176,8 @@ end
 
 function string.match(self, pattern)
   return match(self, u8:decode(pattern));
+end
+
+function string.equals(self, s2)
+  return self == u8:decode(s2);
 end
